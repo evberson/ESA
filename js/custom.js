832d49cbf73c812b76c503e384dc1e003285cca1
David Garry <dgarry@usc.edu>
2017-03-08 21:11:33 -0800

    adding

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,27 @@
-var numBackgrounds = 8;
 function loadBackground() {
-  var num = Math.ceil( Math.random() * numBackgrounds );
+  var numBackgrounds = 23;
+  var num = Math.ceil( Math.random() * (numBackgrounds+1) );
+  if(num > numBackgrounds){
+    num = 15;
+  }
   document.body.background = 'images/bg/img'+num+'.jpg';
   document.body.style.backgroundSize = "cover";// Background repeat
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("navbars").style.display ="none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("navbars").style.display ="block";
+}
+
+$(window).load(function() {
+   setTimeout(function(){
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("fast");
+        $("body").removeClass("preload");
+      }, 1);
+    });
